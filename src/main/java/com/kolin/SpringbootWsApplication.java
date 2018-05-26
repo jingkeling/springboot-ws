0a456d60c227104816863ac7862b8032f6b120c4
jingkeling <841127052@qq.com>
2018-05-26 22:42:04 +0800

    ~

--- src/main/java/com/kolin/SpringbootWsApplication.java
+++ src/main/java/com/kolin/SpringbootWsApplication.java
@@ -3,6 +3,9 @@ package com.kolin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 84112
+ */
 @SpringBootApplication
 public class SpringbootWsApplication {
 
